113539f792272c17c01d213f8ed8e709df7c084b
trujilloML <mateojosuetrujillomelgarejo@gmail.com>
2026-02-27 18:15:19 -0500

    ya funciona para el ddocker

--- product-service/src/main/java/pe/takiq/ecommerce/product_service/config/RedisConfig.java
+++ product-service/src/main/java/pe/takiq/ecommerce/product_service/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package pe.takiq.ecommerce.product_service.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cloud.openfeign.support.PageJacksonModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -19,7 +21,16 @@ public class RedisConfig {
     public RedisCacheConfiguration cacheConfiguration() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        
+        mapper.registerModule(new PageJacksonModule()); 
+        
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        mapper.activateDefaultTyping(
+            mapper.getPolymorphicTypeValidator(),
+            ObjectMapper.DefaultTyping.NON_FINAL,
+            JsonTypeInfo.As.PROPERTY
+        );
 
         return RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(5)) // TTL de 5 minutos
